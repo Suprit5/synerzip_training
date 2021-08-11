2a4eef82ca2534a3851ab1a0f70a9764de63193e
Suprit5 <87975576+Suprit5@users.noreply.github.com>
2021-08-11 13:52:23 +0530

    Delete HackerRank directory

--- HackerRank/string.py
+++
@@ -1,7 +0,0 @@
-"""Without using any string methods, try to print the following: 12345 take a number as an input and generate 
-a string. """
-
-if __name__ == '__main__':
-    n = int(input())
-    for i in range(1,n+1):
-        print(i,end="")
